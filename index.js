7820e909f0bbf6db07dad922b0f8589b5a1d8b50
MarinaKhudolei <marina96khu@mail.ru>
2020-12-24 11:18:29 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+const { PORT = 3000 } = process.env;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
