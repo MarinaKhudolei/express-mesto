5b6dad91f4cb70056a6e72ec5a95b8c23847183e
MarinaKhudolei <marina96khu@mail.ru>
2020-12-24 11:41:37 +0300

    fix index.js via eslinter

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 
+const app = express();
 
 const { PORT = 3000 } = process.env;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 });
